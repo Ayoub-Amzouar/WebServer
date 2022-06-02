f843c9121953df31336ce17a1e54f1faff724612
Ayoub Amzouar <aamzouar@e1r1p1.1337.ma>
2022-06-02 15:43:48 +0100

    SocketClass now can accept connections

--- respond/SocketClass.cpp
+++ respond/SocketClass.cpp
@@ -5,13 +5,28 @@ ft_socket::ft_socket(int domain, int type, int protocol, in_port_t port, in_addr
 {
 	sock_fd = socket(domain, type, protocol);
 
-	address.sin_family = domain;
-	address.sin_port = htons(port);
-	address.sin_addr.s_addr = ip;
+	server_addr.sin_family = domain;
+	server_addr.sin_port = htons(port);
+	server_addr.sin_addr.s_addr = ip;
 
-	if (bind(sock_fd, (const struct sockaddr *)&address, sizeof(address)) < 0)
+	if (bind(sock_fd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
 	{
-		std::cerr << "error:  bind failed\n"; 
+		std::cerr << "error: bind failed\n"; 
 		return ;
+	}
+
+	if (listen(sock_fd, 10) < 0)
+	{
+		std::cerr << "error: listen failed\n"; 
+		exit(EXIT_FAILURE); 
+	}
+}
+
+void	ft_socket::accept_connections( void )
+{
+	if ((accept_fd = accept(sock_fd, (struct sockaddr *)&client_addr, (socklen_t*)&addrlen)) < 0)
+	{
+		std::cerr << "error: accept failed\n"; 
+		exit(EXIT_FAILURE);        
 	}
 }
--- respond/SocketClass.hpp
+++ respond/SocketClass.hpp
@@ -3,10 +3,21 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+/*
+	1) make accept independent function
+	2) write getters for your private attributes
+	3) write both send and receive functions
+*/
+
 class 	ft_socket
 {
 	int					sock_fd;
-	struct sockaddr_in	address;
+	int					accept_fd;
+	struct sockaddr_in	server_addr;
+	struct sockaddr_in	client_addr;
+	socklen_t			addrlen;
 public:
 	ft_socket(int domain, int type, int protocol, in_port_t port, in_addr_t ip);
+
+	void	accept_connections( void );
 };
