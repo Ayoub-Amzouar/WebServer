f742c21bd1f78e3317e69c8627215e60c42640fc
Youssef Noam <2020noam@gmail.com>
2022-06-06 20:08:56 +0100

    finish post

--- cgi/cgi.hpp
+++ cgi/cgi.hpp
@@ -6,24 +6,33 @@
 #include <fcntl.h>
 #include <fstream>
 #include <map>
+#include <utility>
 
 struct Cgi
 {
 private:
     std::string     _path;
     char**          _env;
-    std::string     _file;
+    std::string     _cgi_out_file;
+    std::string     _response_file;
     bool            _error;
 
 private:
-    std::map<std::string, std::string> parse_cgi_output(void);
-    std::vector<std::string> parse_uri(std::string);
-    void error(std::string);
-    void fileInOut(std::string, std::string);
+    int     cgi_status_code(void);
+    int     execute(std::string, std::string);
+    void    error(std::string);
+    void    fileInOut(std::string, std::string);
+    void    generate_response(int);
+    void    send_response(int);
+    std::string get_file(std::string);
+    std::map<std::string, std::string>  parse_cgi_output(void);
+    std::pair<std::string, std::string> parse_uri(std::string);
 
 public:
     Cgi(std::string, char **);
-    int execute(char **, int );
+    int POST(std::string uri, int socket_write, std::string body_file);
+    int GET(int fd);
+    // std::string DELETE();
 };
 
 #endif
