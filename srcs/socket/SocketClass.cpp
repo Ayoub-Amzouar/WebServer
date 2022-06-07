22e447be13e4b5b4d6c579c1c3d6a0142ed1807d
Ayoub Amzouar <aamzouar@e1r13p1.1337.ma>
2022-06-07 18:52:36 +0100

    renaming

---
+++ srcs/socket/SocketClass.cpp
@@ -0,0 +1,65 @@
+#include "../../headers/webserv.hpp"
+
+
+ft_socket::ft_socket(in_port_t port, in_addr_t ip) : accept_fd()
+{
+	sock_fd = socket(PF_INET, SOCK_STREAM, 0);
+
+	server_addr.sin_family = PF_INET;
+	server_addr.sin_port = htons(port);
+	server_addr.sin_addr.s_addr = htons(ip);
+
+	if (bind(sock_fd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	{
+		std::cerr << "error: bind failed\n"; 
+		return ;
+	}
+
+	if (listen(sock_fd, 10) < 0)
+	{
+		std::cerr << "error: listen failed\n"; 
+		exit(EXIT_FAILURE); 
+	}
+}
+
+ft_socket::~ft_socket( void )
+{
+	close(sock_fd);
+	if (accept_fd != 0)
+		close(accept_fd);
+}
+
+void	ft_socket::accept_connections( void )
+{
+	if (accept_fd != 0)
+		close(accept_fd);
+	if ((accept_fd = accept(sock_fd, (struct sockaddr *)&client_addr, (socklen_t*)&addrlen)) < 0)
+	{
+		std::cerr << "error: accept failed\n"; 
+		exit(EXIT_FAILURE);        
+	}
+}
+
+void		ft_socket::response( const std::string& val )
+{
+	send(accept_fd, val.c_str(), val.length(), 0);
+}
+
+std::string	ft_socket::request( void )
+{
+	char		buffer[30000];
+	std::string	res;
+
+	/*ssize_t recv_val = */recv(accept_fd, buffer, 30000, 0);
+	res = buffer;
+
+	return (res);
+}
+
+int					ft_socket::getSockFd( void ) const { return (sock_fd); }
+
+int					ft_socket::getAcceptFd( void ) const { return (accept_fd); }
+
+struct sockaddr_in	ft_socket::getServerAddr( void ) const { return (server_addr); }
+
+struct sockaddr_in	ft_socket::getClientAddr( void ) const { return (client_addr); }
